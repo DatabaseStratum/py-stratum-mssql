a26e29edcac0e506dc6ba272232b85708990e959
Paul Water <p.r.water@setbased.nl>
2016-07-24 19:36:57 +0200

    Initial commit on new repo.

---
+++ test/Row1Test.py
@@ -0,0 +1,40 @@
+"""
+PyStratum
+
+Copyright 2015-2016 Set Based IT Consultancy
+
+Licence MIT
+"""
+from test.DataLayer import DataLayer
+
+from test.StratumTestCase import StratumTestCase
+
+
+class Row1Test(StratumTestCase):
+    # ------------------------------------------------------------------------------------------------------------------
+    def test1(self):
+        """
+        Stored routine with designation type row1 must return 1 row and 1 row only.
+        """
+        ret = DataLayer.tst_test_row1(1)
+        self.assertIsInstance(ret, dict)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test2(self):
+        """
+        An exception must be thrown when a stored routine with designation type row1 returns 0 rows.
+        @expectedException Exception
+        """
+        with self.assertRaises(Exception):
+            DataLayer.tst_test_row1(0)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test3(self):
+        """
+        An exception must be thrown when a stored routine with designation type row1 returns more than 1 rows.
+        @expectedException Exception
+        """
+        with self.assertRaises(Exception):
+            DataLayer.tst_test_row1(2)
+
+# ----------------------------------------------------------------------------------------------------------------------
