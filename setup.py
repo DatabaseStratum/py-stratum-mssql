81351be2415921fe941fca7cff2af8370dc0d556
Paul Water <p.r.water@setbased.nl>
2018-11-16 10:29:02 +0100

    Release: 0.9.24

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='PyStratum-MSSQL',
 
-    version='0.9.23',
+    version='0.9.24',
 
     description='A stored procedure and function loader, wrapper generator for SQL Server',
     long_description=long_description,
