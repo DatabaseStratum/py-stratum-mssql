4959e8cb87b87cb0bb9167ca81b22099298fe43f
Paul Water <p.r.water@setbased.nl>
2019-04-24 07:07:00 +0200

    Release: 0.10.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='PyStratum-MSSQL',
 
-    version='0.9.25',
+    version='0.10.1',
 
     description='A stored procedure and function loader, wrapper generator for SQL Server',
     long_description=long_description,
